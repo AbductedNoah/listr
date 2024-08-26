0f2f044db2042807e14d6dae980fedb5d92032a5
Noah Wells <noah77wells@hotmail.com>
2024-08-26 09:13:29 -0500

    Swagger, first controller

--- Listr/src/main/java/com/nwplus/Listr/ListrApplication.java
+++ Listr/src/main/java/com/nwplus/Listr/ListrApplication.java
@@ -3,11 +3,15 @@ package com.nwplus.Listr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nwplus.Listr.Config.ConnectDatabase;
+
 @SpringBootApplication
 public class ListrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListrApplication.class, args);
+		ConnectDatabase connect = new ConnectDatabase();
+		connect.connectDB();
 	}
 
 }
