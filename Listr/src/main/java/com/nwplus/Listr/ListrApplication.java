cfb334fcb5c192dc57645f666065c56be58bcb36
Noah Wells <noah77wells@hotmail.com>
2024-08-31 18:27:51 -0500

    mostly working

--- Listr/src/main/java/com/nwplus/Listr/ListrApplication.java
+++ Listr/src/main/java/com/nwplus/Listr/ListrApplication.java
@@ -2,16 +2,14 @@ package com.nwplus.Listr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.nwplus.Listr.Config.ConnectDatabase;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin("http://localhost:4200")
 public class ListrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListrApplication.class, args);
-		ConnectDatabase connect = new ConnectDatabase();
-		connect.connectDB();
 	}
 
 }
